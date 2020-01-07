15f2f0bac2abd843e65f9f18c5766dab1bdb4ddf
Jordan Schmidt <57489956+schmi121@users.noreply.github.com>
2020-01-06 21:53:19 -0800

    JS Testing
    Working on rock paper scissors

--- app.js
+++ app.js
@@ -7,7 +7,32 @@ document.addEventListener('DOMContentLoaded', function(){
     let points;
 
     let usersChoice;
-    
+
+    const scoreBoard = {
+        playerScore: 0,
+        cpuScore: 0,
+        incrementPlayerScore: function() {
+            this.playerScore += 1;
+        },
+        incrementCpuScore: function() {
+            this.cpuScore += 1;
+        },
+        winnerCheck: function() {
+            if (this.playerScore === 5) {
+                alert("You Win!");
+                this.resetGame();
+            } else if (this.cpuScore === 5) {
+                alert("You Lose");
+                this.resetGame();
+            }
+        },
+        resetGame: function() {
+            this.playerScore = 0;
+            this.cpuScore = 0;
+            alert('The game will now reset');
+        },
+    };
+
     const rockButton = document.getElementById('rock');
     const scissorsButton = document.getElementById('scissors');
     const paperButton = document.getElementById('paper');
@@ -33,16 +58,21 @@ document.addEventListener('DOMContentLoaded', function(){
         alert(`CPU chose: ${cpuChoice}`)
         if (usersChoice === 'rock' && cpuChoice === 'scissors') {
             alert('You won');
+            scoreBoard.playerScore++
         } else if (usersChoice === 'paper' && cpuChoice === 'rock') {
             alert('You won');
+            scoreBoard.playerScore++
         } else if (usersChoice === 'scissors' && cpuChoice === 'paper') {
             alert('You won'); 
+            scoreBoard.playerScore++
         } else if (usersChoice === cpuChoice) {
             alert('You Tied');
         } else {
             alert('You Lose you dummy');
+            scoreBoard.cpuScore++;
         } 
-
+        alert(`the score is Player: ${scoreBoard.playerScore} to CPU: ${scoreBoard.cpuScore}`);
+        scoreBoard.winnerCheck();
         alert('Cpu is choosing new play');
         cpuChoice = choices[Math.floor(Math.random() * choices.length)];
     }
